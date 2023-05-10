445f0b96ec5f8f3be74caf0ea84ab0b83adfe724
André Filipe Silva Santos <andrefilipe.developer@gmail.com>
2023-05-10 00:02:39 -0300

    teste

--- src/devfordev/setup/CadMac.java
+++ src/devfordev/setup/CadMac.java
@@ -1,7 +1,7 @@
 package devfordev.setup;
 import devfordev.display.DisplayCad;
 public class CadMac{
-	
+	//alteração
 	public static void main(String[] args) {
 		DisplayCad dis = new DisplayCad();
 	}
